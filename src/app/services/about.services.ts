3f9314d801dffb94b4deae964ed0901dd8f6b295
Machine <abderrahim223@gmail.com>
2019-07-17 20:03:17 -0400

    adding new things

---
+++ src/app/services/about.services.ts
@@ -0,0 +1,17 @@
+export class AboutService {
+    
+    Objets = [
+        {
+          name : 'Machine a laver',
+          status: 'allumé'
+        },
+        {
+          name : 'Television',
+          status: 'éteint'
+        },  
+        {
+          name : 'Ordinateur',
+          status: 'allumé'
+        },
+    ];
+}

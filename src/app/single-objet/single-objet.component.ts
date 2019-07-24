5f3775ccd3f4619ca71087df627fec014fc0c52f
Machine <abderrahim223@gmail.com>
2019-07-24 12:23:48 -0400

    adding observable

--- src/app/single-objet/single-objet.component.ts
+++ src/app/single-objet/single-objet.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import{ AboutService }from '../services/about.services';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
  
 @Component({
   selector: 'app-single-objet',
@@ -11,6 +13,7 @@ export class SingleObjetComponent implements OnInit {
 
   name: string = 'Objet';
   status: string = 'Status';
+  secondes: number;
   
   constructor(private ObjetService : AboutService, 
               private route: ActivatedRoute) { }
@@ -19,6 +22,22 @@ export class SingleObjetComponent implements OnInit {
     const id = this.route.snapshot.params['id']; // va chercher le id mais le garde en string
     this.name = this.ObjetService.getObjetById(+id).name; // en ajoutant + on cast le string en nombre et on peux chercher le name
     this.status = this.ObjetService.getObjetById(+id).status;
+    
+    // code pour lobservable qui compte le temps quon reste sur cette page 
+    // subscribe peut prendre jusqua 3 arguments
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+      (value) => {
+        this.secondes = value;
+        console.log("L'observable commence à compter!");
+      },
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
   }
 
 }
